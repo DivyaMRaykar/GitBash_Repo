0f2dd33457a5c193a334e063d9e848d3fa2eec72
DivyaMRaykar <divyamraykar@gmail.com>
2025-03-11 15:52:09 +0530

    New Commit

---
+++ src/test/java/gitbash/practise/CreateLeadsTest.java
@@ -0,0 +1,12 @@
+package gitbash.practise;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadsTest {
+
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("-- Leads Created --");
+	}
+}
